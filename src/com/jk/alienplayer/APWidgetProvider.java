55490fd9631c392f8d8b9b3f1a1e0bb96b2cb86d
jacky0karl <jacky0karl@gmail.com>
2014-06-17 16:11:51 +0800

    add appwidget

---
+++ src/com/jk/alienplayer/APWidgetProvider.java
@@ -0,0 +1,42 @@
+package com.jk.alienplayer;
+
+import com.jk.alienplayer.data.PlayingInfoHolder;
+import com.jk.alienplayer.ui.MainActivity;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.widget.RemoteViews;
+
+public class APWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            appWidgetManager.updateAppWidget(appWidgetIds[i], syncView(context));
+        }
+    }
+
+    private RemoteViews syncView(Context context) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
+        views.setOnClickPendingIntent(R.id.artwork, buildIntent(context));
+
+        Bitmap artwork = PlayingInfoHolder.getInstance().getPlaybarArtwork();
+        if (artwork == null) {
+            views.setImageViewResource(R.id.artwork, R.drawable.ic_launcher);
+        } else {
+            views.setImageViewBitmap(R.id.artwork, artwork);
+        }
+        return views;
+    }
+
+    private PendingIntent buildIntent(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+}
