90ad69a5e2639a53bb20b764667b0f6ec6a04d6c
jacky0karl <jacky0karl@gmail.com>
2014-07-13 11:26:09 +0800

    modify search layout

--- src/com/jk/alienplayer/ui/adapter/SearchResultsAdapter.java
+++ src/com/jk/alienplayer/ui/adapter/SearchResultsAdapter.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.jk.alienplayer.R;
+import com.jk.alienplayer.metadata.AlbumInfo;
+import com.jk.alienplayer.metadata.SongInfo;
 import com.jk.alienplayer.metadata.SearchResult;
 
 import android.content.Context;
@@ -11,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SearchResultsAdapter extends BaseAdapter {
@@ -49,36 +52,100 @@ public class SearchResultsAdapter extends BaseAdapter {
     }
 
     @Override
+    public int getItemViewType(int position) {
+        return mResults.get(position).type;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 3;
+    }
+
+    @Override
     public View getView(int position, View view, ViewGroup parent) {
-        ViewHolder viewHolder = null;
+        int type = getItemViewType(position);
+        switch (type) {
+        case SearchResult.TYPE_ARTISTS:
+            return getArtistView(position, view);
+        case SearchResult.TYPE_ALBUMS:
+            return getAlbumView(position, view);
+        case SearchResult.TYPE_TRACKS:
+            return getTrackView(position, view);
+        default:
+            return view;
+        }
+    }
+
+    private View getArtistView(int position, View view) {
+        ArtistViewHolder viewHolder = null;
         if (view == null) {
-            viewHolder = new ViewHolder();
-            view = mInflater.inflate(R.layout.search_item, null);
+            viewHolder = new ArtistViewHolder();
+            view = mInflater.inflate(R.layout.search_artist_item, null);
             viewHolder.title = (TextView) view.findViewById(R.id.title);
             viewHolder.content = (TextView) view.findViewById(R.id.content);
             view.setTag(viewHolder);
         } else {
-            viewHolder = (ViewHolder) view.getTag();
+            viewHolder = (ArtistViewHolder) view.getTag();
         }
 
         SearchResult result = mResults.get(position);
         viewHolder.content.setText(result.data.getDisplayName());
-        if (position == 0) {
-            showTitle(viewHolder.title, result.type);
-        } else if (mResults.get(position - 1).type != result.type) {
-            showTitle(viewHolder.title, result.type);
-        } else {
-            viewHolder.title.setVisibility(View.GONE);
-        }
+        showTitle(viewHolder.title, position, result.type);
         return view;
     }
 
-    static class ViewHolder {
-        TextView title;
-        TextView content;
+    private View getAlbumView(int position, View view) {
+        AlbumViewHolder viewHolder = null;
+        if (view == null) {
+            viewHolder = new AlbumViewHolder();
+            view = mInflater.inflate(R.layout.search_album_item, null);
+            viewHolder.title = (TextView) view.findViewById(R.id.title);
+            viewHolder.content = (TextView) view.findViewById(R.id.content);
+            viewHolder.artist = (TextView) view.findViewById(R.id.artist);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (AlbumViewHolder) view.getTag();
+        }
+
+        SearchResult result = mResults.get(position);
+        viewHolder.content.setText(result.data.getDisplayName());
+        viewHolder.artist.setText(((AlbumInfo) result.data).artist);
+        showTitle(viewHolder.title, position, result.type);
+        return view;
     }
 
-    private void showTitle(TextView view, int type) {
+    private View getTrackView(int position, View view) {
+        TrackViewHolder viewHolder = null;
+        if (view == null) {
+            viewHolder = new TrackViewHolder();
+            view = mInflater.inflate(R.layout.search_track_item, null);
+            viewHolder.title = (TextView) view.findViewById(R.id.title);
+            viewHolder.content = (TextView) view.findViewById(R.id.content);
+            viewHolder.artists = (TextView) view.findViewById(R.id.artist);
+            viewHolder.action = (ImageView) view.findViewById(R.id.action);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (TrackViewHolder) view.getTag();
+        }
+
+        SearchResult result = mResults.get(position);
+        viewHolder.content.setText(result.data.getDisplayName());
+        viewHolder.artists.setText(((SongInfo) result.data).artist);
+        showTitle(viewHolder.title, position, result.type);
+        return view;
+    }
+
+    private void showTitle(TextView titleView, int position, int type) {
+        if (position == 0) {
+            doShowTitle(titleView, type);
+        } else if (mResults.get(position - 1).type != type) {
+            doShowTitle(titleView, type);
+        } else {
+            titleView.setVisibility(View.GONE);
+        }
+    }
+
+    private void doShowTitle(TextView titleView, int type) {
         String title = null;
         switch (type) {
         case SearchResult.TYPE_ARTISTS:
@@ -94,7 +161,25 @@ public class SearchResultsAdapter extends BaseAdapter {
             break;
         }
 
-        view.setVisibility(View.VISIBLE);
-        view.setText(title);
+        titleView.setVisibility(View.VISIBLE);
+        titleView.setText(title);
+    }
+
+    static class ArtistViewHolder {
+        TextView title;
+        TextView content;
+    }
+
+    static class AlbumViewHolder {
+        TextView title;
+        TextView content;
+        TextView artist;
+    }
+
+    static class TrackViewHolder {
+        TextView title;
+        TextView content;
+        TextView artists;
+        ImageView action;
     }
 }
