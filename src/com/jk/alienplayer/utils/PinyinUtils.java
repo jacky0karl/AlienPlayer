618838d9ff1ca8de04d2b6e8a97a7dfdb2cade4f
jacky0karl <jacky0karl@gmail.com>
2014-07-29 17:08:11 +0800

    replace pinyin lib with android HanziToPinyin

--- src/com/jk/alienplayer/utils/PinyinUtils.java
+++ src/com/jk/alienplayer/utils/PinyinUtils.java
@@ -1,38 +1,23 @@
 package com.jk.alienplayer.utils;
 
-import net.sourceforge.pinyin4j.PinyinHelper;
-import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
-import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+import java.util.ArrayList;
+
+import com.jk.alienplayer.utils.HanziToPinyin.Token;
 
 public class PinyinUtils {
-    private static HanyuPinyinOutputFormat sFormat = new HanyuPinyinOutputFormat();
 
-    public PinyinUtils() {
-        sFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-    }
-
-    public String getPinyinString(String input) {
+    public static String getPinyinString(String input) {
+        ArrayList<Token> tokens = HanziToPinyin.getInstance().get(input);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            sb.append(getPinyin(ch));
+        if (tokens != null && tokens.size() > 0) {
+            for (Token token : tokens) {
+                if (Token.PINYIN == token.type) {
+                    sb.append(token.target);
+                } else {
+                    sb.append(token.source);
+                }
+            }
         }
-        return sb.toString();
-    }
-
-    public String getPinyin(char ch) {
-        String[] pinyin = null;
-        try {
-            pinyin = PinyinHelper.toHanyuPinyinStringArray(ch, sFormat);
-        } catch (BadHanyuPinyinOutputFormatCombination e) {
-            e.printStackTrace();
-        }
-
-        if (pinyin != null) {
-            return pinyin[0];
-        } else {
-            return String.valueOf(ch);
-        }
+        return sb.toString().toLowerCase();
     }
 }
